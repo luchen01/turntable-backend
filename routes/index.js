bfa1f20a2e13b79f6fc5eb6c274a4d94306f0efe
Andrew Marten Medina <ammy2015@mymail.pomona.edu>
2017-10-20 14:34:37 -0700

    something happened

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+
+const SpotifyStrategy = require('passport-spotify').Strategy;
+
+passport.use(new SpotifyStrategy({
+    clientID: process.env.SPOTIFY_CLIENT_ID,
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/spotify/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate({ spotifyId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
+
+router.get('/auth/spotify',
+  passport.authenticate('spotify'),
+  function(req, res){
+    // The request will be redirected to spotify for authentication, so this
+    // function will not be called.
+  });
+
+router.get('/auth/spotify/callback',
+  passport.authenticate('spotify', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
