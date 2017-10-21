38f16d2ac9f769376c7bfb2f44d9e55d553285eb
luchen01 <luchenpengnyc@gmail.com>
2017-10-20 21:46:49 -0700

    fix heroku

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ const SpotifyStrategy = require('passport-spotify').Strategy;
 passport.use(new SpotifyStrategy({
     clientID: process.env.SPOTIFY_CLIENT_ID,
     clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-    callbackURL: "http://localhost:8888/callback"
+    callbackURL: "https://turntableapp.herokuapp.com/auth/spotify/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     var access = accessToken;
